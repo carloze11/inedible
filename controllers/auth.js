c607bcbc5453400e473fff3f1b03972b912eee12
Carlos Estrada <cestrada1109@gmail.com>
2022-11-05 10:46:10 -0500

    Add sign up logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,11 +58,58 @@ exports.getLogout = (req, res) => {
 };
 
 exports.getSignup = (req, res) => {
+    if (req.user) {
+        return res.redirect("/profile");
+    }
     res.render("signup", {
         title: "Signup",
     });
 };
 
-exports.postSignup = (req, res) => {
-    res.send("This is the signup post page");
+exports.postSignup = (req, res, next) => {
+    const validationErrors = [];
+    if (!validator.isEmpty(req.body.username))
+        validationErrors.push({ msg: "Please enter a valid email address." });
+    if (!validator.isLength(req.body.password, { min: 8 }))
+        validationErrors.push({
+            msg: "Password must be at least 8 characters long",
+        });
+    if (req.body.password !== req.body.confirmPassword)
+        validationErrors.push({ msg: "Passwords do not match " });
+
+    if (validationErrors.length) {
+        req.flash("errors", validationErrors);
+        return res.redirect("../signup");
+    }
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    User.findOne(
+        { $or: [{ username: req.body.username }] },
+        (err, existingUser) => {
+            if (err) {
+                return next(err);
+            }
+            if (existingUser) {
+                req.flash("errors", {
+                    msg: "Account with that username already exists.",
+                });
+                return res.redirect("../signup");
+            }
+            user.save((err) => {
+                if (err) {
+                    return next(err);
+                }
+                req.logIn(user, (err) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.redirect("/profile");
+                });
+            });
+        }
+    );
 };
