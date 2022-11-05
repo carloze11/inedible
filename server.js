5de336e0dd0d51f7f55a47c605c27d6815f8a50e
Carlos Estrada <cestrada1109@gmail.com>
2022-11-05 11:39:47 -0500

    Add MongoDB

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-layouts')
-const methodOverride = require('method-override')
-const mongoose = require('mongoose')
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+const expressLayouts = require("express-layouts");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 
+//load config
+dotenv.config({ path: "./config/config.env" });
+const PORT = process.env.PORT || 5000;
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layout/layout");
+app.use(expressLayouts);
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layout/layout')
-app.use(expressLayouts)
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
+const indexRouter = require("./routes/index");
 
+app.use("/", indexRouter);
 
-const indexRouter = require('./routes/index')
-
-app.use('/', indexRouter)
-
-app.listen(3000, () => {
-    console.log(`Server running at port 3000`)
-})
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+});
