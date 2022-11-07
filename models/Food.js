16fd4eff3821b74c0471d44ee12447a09f8f6126
Carlos Estrada <cestrada1109@gmail.com>
2022-11-06 23:13:17 -0600

    Add foodCategory to schema

--- models/Food.js
+++ models/Food.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const FoodSchema = new Schema({
-    name: { type: String, required: true },
+    foodName: { type: String, required: true },
+    foodCategory: { type: String, enum: [Dairy, gluten, nut] },
     ingredients: { type: String, required: true },
     image: { type: String },
     createdAt: { type: Date, default: Date.now },
