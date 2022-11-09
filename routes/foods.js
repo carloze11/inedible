d16a177fe041a8d75be8921d1b3a93719ce59f92
Carlos Estrada <cestrada1109@gmail.com>
2022-11-08 21:57:17 -0600

    Add helper functions to remove tags from ingredients and cap chars

--- routes/foods.js
+++ routes/foods.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 const Food = require("../models/Food");
+const User = require("../models/User");
 
 router.get("/add", ensureAuth, (req, res) => {
     res.render("foods/add");
@@ -14,6 +15,7 @@ router.post("/", ensureAuth, async (req, res) => {
             foodName: req.body.foodName,
             foodCategory: req.body.foodCategory,
             ingredients: req.body.ingredients,
+            user: req.body.user,
             image: req.body.image,
         });
         console.log("Food created successfully.");
@@ -23,5 +25,40 @@ router.post("/", ensureAuth, async (req, res) => {
         res.render("error/500");
     }
 });
+
+// Show food by everyone
+router.get("/", ensureAuth, async (req, res) => {
+    try {
+        const foods = await Food.find()
+            .populate("user")
+            .sort({ foodName: "asc" })
+            .lean();
+        console.log(foods);
+        res.render("foods/index", {
+            foods,
+            truncate: truncate,
+            removeTags: removeTags,
+        });
+    } catch (err) {
+        console.log(err);
+        res.render("error/500");
+    }
+});
+
+// helper functions to be moved to controller along with cbs
+const truncate = (str, len) => {
+    if (str.length > len && str.length > 0) {
+        let newStr = str + " ";
+        newStr = str.substr(0, len);
+        newStr = str.substr(0, newStr.lastIndexOf(" "));
+        newStr = newStr.length > 0 ? newStr : str.substr(0, len);
+        return newStr + "...";
+    }
+    return str;
+};
+
+const removeTags = (input) => {
+    return input.replace(/<(?:.|\n)*?>/gm, "");
+};
 
 module.exports = router;
