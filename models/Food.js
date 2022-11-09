86cffa95647afc595023a29f0d8b5c9eb89ff427
Carlos Estrada <cestrada1109@gmail.com>
2022-11-08 21:58:12 -0600

    Add users to schema and capital categories

--- models/Food.js
+++ models/Food.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const FoodSchema = new Schema({
     foodName: { type: String, required: true },
-    foodCategory: { type: String, enum: ["dairy", "gluten", "nut"] },
+    foodCategory: { type: String, enum: ["Dairy", "Gluten", "Nut"] },
     ingredients: { type: String, required: true },
     image: { type: String },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
     createdAt: { type: Date, default: Date.now },
 });
 
