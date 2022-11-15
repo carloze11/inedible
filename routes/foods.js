78a9ab9d9e2564cb4546fdb1f959d76b19b3a2d1
Carlos Estrada <cestrada1109@gmail.com>
2022-11-14 23:48:46 -0600

    Remove show more variables till reactification

--- routes/foods.js
+++ routes/foods.js
@@ -56,16 +56,14 @@ router.get("/search", ensureAuth, async (req, res) => {
 router.post("/results", ensureAuth, async (req, res) => {
     try {
         let productSearch = req.body.productSearch;
-        let showMore = req.body.showMore || 0;
-        console.log(`This is ${showMore}`);
-        const api_url = `https://api.spoonacular.com/food/products/search?query=${productSearch}&apiKey=${process.env.SPOON_API_KEY}`;
+        const api_url = `https://api.spoonacular.com/food/products/search?query=${productSearch}&number=100&apiKey=${process.env.SPOON_API_KEY}`;
         const data = await fetch(api_url);
         const json = await data.json();
         res.render("foods/results", {
             productSearch: productSearch,
             items: json.products,
             total: json.totalProducts,
-            number: json.number + showMore,
+            number: json.number,
         });
     } catch (err) {
         console.error(err);
