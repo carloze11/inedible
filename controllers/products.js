ee69b38804ed63deece03d516325e990dc770165
Carlos Estrada <cestrada1109@gmail.com>
2022-12-07 22:55:55 -0600

    Remove ejs files from products folder and add showProduct.jsx

--- controllers/products.js
+++ controllers/products.js
@@ -32,7 +32,7 @@ exports.getProduct = async (req, res) => {
         const api_url = `https://api.spoonacular.com/food/products/${productId}?apiKey=${process.env.SPOON_API_KEY}`;
         const data = await fetch(api_url);
         const json = await data.json();
-        res.render("products/show-product", {
+        res.render("products/showProduct", {
             title: json.title,
             description: json.description,
             image: json.image,
---
+++ views/products/showProduct.jsx
@@ -0,0 +1,22 @@
+import Main from "../components/Main";
+
+export default function ShowProduct({ title, image, description }) {
+    return (
+        <Main>
+            <div className="row">
+                <div className="col s12 m8">
+                    <h3>{title}</h3>
+                    <div>
+                        <img src={image} alt="" />
+                    </div>
+                    <div className="card story">
+                        <div className="card-content">
+                            {/* <span className="card-title"> {{formatDate date ' MMMM Do YYYY, h:mm:ss a'}} </span>  */}
+                            {description}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Main>
+    );
+}
