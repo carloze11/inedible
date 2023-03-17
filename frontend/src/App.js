7160637be878dbc3a34b26bf3fc1fcf43e5096a1
Carlos Estrada <cestrada1109@gmail.com>
2023-03-17 13:25:20 -0500

    Add logic to display diff navbar depending on user status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Navbar />
+                <Navbar user={user} />
                 <Routes>
                     <Route
                         path="/"
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import M from "materialize-css";
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar({ user }) {
     const sidenavRef = useRef(null);
 
     useEffect(() => {
@@ -16,44 +16,56 @@ export default function Navbar() {
 
     return (
         <nav className="grey darken-3">
-            <div className="nav-wrapper container">
-                <Link
-                    to="/"
-                    className="brand-logo center"
-                    onClick={closeSidenav}
-                >
-                    <i className="fa-solid fa-skull-crossbones"></i>InEdible
-                </Link>
-                <a
-                    href="#"
-                    data-target="mobile-demo"
-                    className="sidenav-trigger show-on-large"
-                >
-                    <i className="fas fa-bars"></i>
-                </a>
-                <ul id="mobile-demo" className="sidenav" ref={sidenavRef}>
-                    <li>
-                        <Link to="/dashboard" onClick={closeSidenav}>
-                            Dashboard
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/foods" onClick={closeSidenav}>
-                            Community Food
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/products/search" onClick={closeSidenav}>
-                            Search Products
-                        </Link>
-                    </li>
-                    <li>
-                        <a href="/auth/logout" onClick={closeSidenav}>
-                            Logout
-                        </a>
-                    </li>
-                </ul>
-            </div>
+            {!user ? (
+                <div>
+                    <Link
+                        to="/"
+                        className="brand-logo center"
+                        onClick={closeSidenav}
+                    >
+                        <i className="fa-solid fa-skull-crossbones"></i>InEdible
+                    </Link>
+                </div>
+            ) : (
+                <div className="nav-wrapper container">
+                    <Link
+                        to="/"
+                        className="brand-logo center"
+                        onClick={closeSidenav}
+                    >
+                        <i className="fa-solid fa-skull-crossbones"></i>InEdible
+                    </Link>
+                    <a
+                        href="#"
+                        data-target="mobile-demo"
+                        className="sidenav-trigger show-on-large"
+                    >
+                        <i className="fas fa-bars"></i>
+                    </a>
+                    <ul id="mobile-demo" className="sidenav" ref={sidenavRef}>
+                        <li>
+                            <Link to="/dashboard" onClick={closeSidenav}>
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/foods" onClick={closeSidenav}>
+                                Community Food
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/products/search" onClick={closeSidenav}>
+                                Search Products
+                            </Link>
+                        </li>
+                        <li>
+                            <a href="/auth/logout" onClick={closeSidenav}>
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </nav>
     );
 }
