c69f6d91d867fb98de1d2504d260cab478916eb8
Carlos Estrada <cestrada1109@gmail.com>
2023-03-20 23:15:51 -0500

    Add logic to navbar to show/hide menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Navbar />
+                <Navbar user={user} />
                 <Routes>
                     <Route
                         path="/"
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import useLogout from "../hooks/useLogout";
 
-export default function Navbar() {
+export default function Navbar({ user }) {
     const { logout } = useLogout();
     const sidenavRef = useRef(null);
 
@@ -18,51 +18,76 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="grey darken-3">
-            <div className="nav-wrapper container">
-                <Link
-                    to="/"
-                    className="brand-logo center"
-                    onClick={closeSidenav}
-                >
-                    <i className="fa-solid fa-skull-crossbones"></i>InEdible
-                </Link>
-                <a
-                    href="#"
-                    data-target="mobile-demo"
-                    className="sidenav-trigger show-on-large"
-                >
-                    <i className="fas fa-bars"></i>
-                </a>
-                <ul id="mobile-demo" className="sidenav" ref={sidenavRef}>
-                    <li>
-                        <Link to="/dashboard" onClick={closeSidenav}>
-                            Dashboard
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/products/search" onClick={closeSidenav}>
-                            Search Products
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/account" onClick={closeSidenav}>
-                            My Account
-                        </Link>
-                    </li>
-                    <li>
+        <div>
+            {!user ? (
+                <nav className="grey darken-3">
+                    <div className="nav-wrapper container">
                         <Link
                             to="/"
-                            onClick={() => {
-                                logout();
-                                closeSidenav();
-                            }}
+                            className="brand-logo center"
+                            onClick={closeSidenav}
                         >
-                            Logout
+                            <i className="fa-solid fa-skull-crossbones"></i>
+                            InEdible
                         </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+                    </div>
+                </nav>
+            ) : (
+                <nav className="grey darken-3">
+                    <div className="nav-wrapper container">
+                        <Link
+                            to="/"
+                            className="brand-logo center"
+                            onClick={closeSidenav}
+                        >
+                            <i className="fa-solid fa-skull-crossbones"></i>
+                            InEdible
+                        </Link>
+                        <a
+                            href="#"
+                            data-target="mobile-demo"
+                            className="sidenav-trigger show-on-large"
+                        >
+                            <i className="fas fa-bars"></i>
+                        </a>
+                        <ul
+                            id="mobile-demo"
+                            className="sidenav"
+                            ref={sidenavRef}
+                        >
+                            <li>
+                                <Link to="/dashboard" onClick={closeSidenav}>
+                                    Dashboard
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/products/search"
+                                    onClick={closeSidenav}
+                                >
+                                    Search Products
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/account" onClick={closeSidenav}>
+                                    My Account
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/"
+                                    onClick={() => {
+                                        logout();
+                                        closeSidenav();
+                                    }}
+                                >
+                                    Logout
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            )}
+        </div>
     );
 }
