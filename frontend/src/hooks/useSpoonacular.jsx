e6404ee5662aa2e2d14c182f5f2e20a527dbb120
Carlos Estrada <cestrada1109@gmail.com>
2023-03-21 22:56:35 -0500

    Create working Spoonacular hook with product search

--- frontend/src/hooks/useSpoonacular.jsx
+++ frontend/src/hooks/useSpoonacular.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 export const useSpoonacular = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const searchSpoon = async (product) => {
+    const searchSpoon = async (productSearch) => {
         const response = await fetch("/api/products/results", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ product }),
+            body: JSON.stringify({ productSearch }),
         });
 
-        const json = await json.response();
+        const json = await response.json();
+        console.log(json);
 
         if (!response.ok) {
             setIsLoading(false);
--- frontend/src/pages/ProductSearch.jsx
+++ frontend/src/pages/ProductSearch.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+import { useSpoonacular } from "../hooks/useSpoonacular";
+
 const ProductSearch = () => {
+    const [query, setQuery] = useState("");
+    const { searchSpoon } = useSpoonacular();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        searchSpoon(query);
+    };
+
     return (
         <div className="container">
             <h1 className="center-align">
                 Search for food using the Spoonacular API!
             </h1>
-            <form action="/products/results" method="POST">
+            <form onSubmit={handleSubmit}>
                 <div className="row">
                     <label htmlFor="productSearch">Search</label>
                     <input
                         type="text"
                         name="productSearch"
                         id="productSearch"
+                        onChange={(e) => setQuery(e.target.value)}
                         required
                     />
                     <input type="submit" className="btn" />
