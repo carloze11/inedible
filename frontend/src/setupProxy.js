62c28ada7795a92c881a8b41ab47bedbaf9603da
Carlos Estrada <cestrada1109@gmail.com>
2023-03-21 22:35:34 -0500

    Change proxy to accept all api routes

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,14 +2,14 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        ["/api", "/auth/google"],
+        "/api/**",
         createProxyMiddleware({
             target: "http://localhost:4000",
             changeOrigin: true,
         })
     );
     app.use(
-        ["/api", "/auth/logout"],
+        ["/auth/google", "/auth/logout"],
         createProxyMiddleware({
             target: "http://localhost:4000",
             method: "POST",
