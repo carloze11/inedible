be62ddbd77aaa08ccf299e639335c4cf278db344
Carlos Estrada <cestrada1109@gmail.com>
2023-03-22 22:06:45 -0500

    Return loading status and query data

--- frontend/src/hooks/useSpoonacular.jsx
+++ frontend/src/hooks/useSpoonacular.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
 export const useSpoonacular = () => {
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [queryData, setQueryData] = useState(null);
 
     const searchSpoon = async (productSearch) => {
+        setIsLoading(true);
+
         const response = await fetch("/api/products/results", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -19,8 +22,9 @@ export const useSpoonacular = () => {
             setError(json.error);
         }
         if (response.ok) {
+            setQueryData(json);
             setIsLoading(false);
         }
     };
-    return { searchSpoon, isLoading, error };
+    return { searchSpoon, queryData, isLoading, error };
 };
