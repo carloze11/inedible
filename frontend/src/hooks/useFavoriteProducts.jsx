467e5b311ca0d8b09308b63ebd2c7476d70f65fe
Carlos Estrada <cestrada1109@gmail.com>
2023-04-03 11:27:31 -0500

    Add logic to render favorite products to dashboard

--- frontend/src/hooks/useFavoriteProducts.jsx
+++ frontend/src/hooks/useFavoriteProducts.jsx
@@ -3,26 +3,33 @@ import { useState } from "react";
 export const useFavoriteProducts = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [favProducts, setFavProducts] = useState(null);
+    const [favoriteProducts, setFavoriteProducts] = useState(null);
 
     const getFavoriteProducts = async () => {
         setIsLoading(true);
 
         const response = await fetch(`/api/products/get-favorite-products`, {
-            method: "GET",
+            method: "POST",
             headers: { "Content-Type": "application/json" },
         });
 
         const json = await response.json();
 
+        console.log(json);
         if (!response.ok) {
             setIsLoading(false);
             setError(json.error);
         }
         if (response.ok) {
-            setFavProducts(json);
+            setFavoriteProducts(json);
             setIsLoading(false);
         }
     };
-    return { getFavoriteProducts, favProducts, isLoading, setIsLoading, error };
+    return {
+        getFavoriteProducts,
+        favoriteProducts,
+        isLoading,
+        setIsLoading,
+        error,
+    };
 };
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -15,70 +15,72 @@ export default function Dashboard(props) {
         getFavoriteProducts();
     }, []);
 
+    const renderFavorites = () => {
+        return !isLoading && favoriteProducts ? (
+            <table className="striped">
+                <thead>
+                    <tr>
+                        <th>Food</th>
+                        <th>Category</th>
+                        <th>Ingredients</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {favoriteProducts.map((product) => {
+                        return (
+                            <tr key={product.productId}>
+                                <td>
+                                    <a href={`/foods/`}>
+                                        {product.productName}
+                                    </a>
+                                </td>
+                                <td>
+                                    <span className="dash-status">
+                                        {product.productDescription}
+                                    </span>
+                                </td>
+                                <td>
+                                    <a
+                                        href={`/foods/edit/`}
+                                        className="btn btn-float"
+                                    >
+                                        <i className="fas fa-edit"></i>
+                                    </a>
+
+                                    <form
+                                        action={`/foods/`}
+                                        method="POST"
+                                        id="delete-form"
+                                    >
+                                        <input
+                                            type="hidden"
+                                            name="_method"
+                                            value="DELETE"
+                                        />
+                                        <button
+                                            type="submit"
+                                            className="btn red"
+                                        >
+                                            <i className="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        ) : (
+            <div className="card ">You got no food!</div>
+        );
+    };
+
     return (
         <div className="container">
             <h1 className="center">Dashboard</h1>
             <h4>Saved Products</h4>
-
-            {favoriteProducts ? (
-                <table className="striped">
-                    <thead>
-                        <tr>
-                            <th>Food</th>
-                            <th>Category</th>
-                            <th>Ingredients</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {favoriteProducts.map((product) => {
-                            return (
-                                <tr key={product.productId}>
-                                    <td>
-                                        <a href={`/foods/${food._id}`}>
-                                            {product.productName}
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <span className="dash-status">
-                                            {product.productDescription}
-                                        </span>
-                                    </td>
-                                    <td>
-                                        <a
-                                            href={`/foods/edit/${food._id}`}
-                                            className="btn btn-float"
-                                        >
-                                            <i className="fas fa-edit"></i>
-                                        </a>
-
-                                        <form
-                                            action={`/foods/${food._id}`}
-                                            method="POST"
-                                            id="delete-form"
-                                        >
-                                            <input
-                                                type="hidden"
-                                                name="_method"
-                                                value="DELETE"
-                                            />
-                                            <button
-                                                type="submit"
-                                                className="btn red"
-                                            >
-                                                <i className="fas fa-trash"></i>
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            ) : (
-                <div className="card ">You got no food!</div>
-            )}
-            <Addbtn />
+            {renderFavorites()}
         </div>
     );
 }
